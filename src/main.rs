4327e91c65ca173f21b16f8535bc3318cf978398
Ben Vazzana <benvazzana@gmail.com>
2025-09-12 17:15:28 -0400

    feat: add arg parser for CLI
    - parse two arguments: the audio file path, and the number of semitones to shift
    - enforce pitch shift to be within range [-12, 12]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name="audi8", version="1.0", about="A CLI audio transposition tool", long_about = None)]
+struct Args {
+    #[arg(index=1)]
+    file: String,
+
+    #[arg(index=2, allow_hyphen_values = true,
+          value_parser = clap::value_parser!(i32).range(-12..=12))]
+    num_semitones: i32
 }
+
+fn main() {
+    let args = Args::parse();
+
+    println!("file path: {}", args.file);
+    println!("pitch shift: {}", args.num_semitones);
+}
+
