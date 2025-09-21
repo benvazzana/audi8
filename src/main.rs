ade0dee64032dbef5032bf640021198acbe4213b
Ben Vazzana <benvazzana@gmail.com>
2025-09-21 14:11:59 -0400

    feat: add api as subcommand to cli

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,110 @@
-use clap::Parser;
+use clap::{Subcommand, Parser};
 use hound::{Error, WavReader, WavSpec, WavWriter};
 
 use audi8::{error::InsufficientInputError, time_scaler::TimeScaler, wav};
 use rubato::{FftFixedInOut, Resampler};
 
 #[derive(Parser)]
-#[command(name="audi8", version="1.0", about="A CLI audio transposition tool", long_about = None)]
+#[command(name="audi8", version="1.0", about="An audio transposition tool", long_about = None)]
 struct Args {
-    #[arg(index=1)]
-    file: String,
+    // Allows for both CLI and API server functionality
+    #[command(subcommand)]
+    command: Command,
+}
 
-    #[arg(index=2, allow_hyphen_values = true,
-          value_parser = clap::value_parser!(i32).range(-12..=12))]
-    num_semitones: i32,
+#[derive(Subcommand)]
+enum Command {
+    Cli {
+        #[arg(index=1)]
+        input_file: String,
 
-    #[arg(short, long, default_value_t = String::from("output.wav"))]
-    output_file: String,
+        #[arg(index=2, allow_hyphen_values = true,
+              value_parser = clap::value_parser!(i32).range(-12..=12))]
+        num_semitones: i32,
+
+        #[arg(short, long, default_value = "output.wav")]
+        output_file: String,
+    },
+
+    Serve {
+        #[arg(short, long, default_value = "127.0.0.1:8080")]
+        bind: String,
+    },
 }
 
 fn main() {
     let args = Args::parse();
+    match args.command {
+        Command::Cli { input_file, num_semitones, output_file } => {
+            println!("file path: {input_file}");
+            println!("pitch shift: {num_semitones}");
 
-    let file_path = &args.file;
-    let pitch_shift = args.num_semitones;
-    let output_file = &args.output_file;
+            let mut reader = match WavReader::open(&input_file) {
+                Ok(reader) => reader,
+                Err(Error::FormatError(..)) => panic!("audio must be a valid WAV file"),
+                Err(err) => panic!("could not open audio file: {err}")
+            };
+            let spec = reader.spec();
+            println!("spec: {spec:?}");
 
-    println!("file path: {file_path}");
-    println!("pitch shift: {pitch_shift}");
+            let mut writer = WavWriter::create(
+                &output_file,
+                WavSpec {
+                    channels: spec.channels,
+                    sample_rate: spec.sample_rate,
+                    bits_per_sample: 16,
+                    sample_format: hound::SampleFormat::Int,
+                },
+            ).unwrap();
 
-    let mut reader = match WavReader::open(file_path) {
-        Ok(reader) => reader,
-        Err(Error::FormatError(..)) => panic!("audio must be a valid WAV file"),
-        Err(err) => panic!("could not open audio file: {err}")
-    };
-    let spec = reader.spec();
-    println!("spec: {spec:?}");
+            let chunk_size = 4096usize;
+            let sampling_ratio = 2f32.powf(num_semitones as f32 / 12.0);
 
-    let mut writer = WavWriter::create(
-        output_file,
-        WavSpec {
-            channels: spec.channels,
-            sample_rate: spec.sample_rate,
-            bits_per_sample: 16,
-            sample_format: hound::SampleFormat::Int,
-        },
-    ).unwrap();
+            let mut resampler = FftFixedInOut::<f32>::new(
+                spec.sample_rate as usize,
+                (spec.sample_rate as f32 / sampling_ratio) as usize,
+                chunk_size,
+                spec.channels as usize,
+            ).unwrap();
 
-    let chunk_size = 4096usize;
-    let sampling_ratio = 2f32.powf(pitch_shift as f32 / 12.0);
+            let mut time_scaler = TimeScaler::new(
+                chunk_size,
+                chunk_size / 2,
+                sampling_ratio,
+                spec.channels as usize,
+            );
 
-    let mut resampler = FftFixedInOut::<f32>::new(
-        spec.sample_rate as usize,
-        (spec.sample_rate as f32 / sampling_ratio) as usize,
-        chunk_size,
-        spec.channels as usize,
-    ).unwrap();
+            // Read entire file into input buffer
+            loop {
+                let in_block = wav::read_frames(&mut reader, chunk_size);
+                let exhausted = in_block.iter().any(|channel| channel.len() < chunk_size);
+                time_scaler.push_block(&in_block);
 
-    let mut time_scaler = TimeScaler::new(
-        chunk_size,
-        chunk_size / 2,
-        sampling_ratio,
-        spec.channels as usize,
-    );
-
-    // Read entire file into input buffer
-    loop {
-        let in_block = wav::read_frames(&mut reader, chunk_size);
-        let exhausted = in_block.iter().any(|channel| channel.len() < chunk_size);
-        time_scaler.push_block(&in_block);
-
-        if exhausted {
-            break;
-        }
-    }
-
-    loop {
-        let in_frames = resampler.input_frames_next();
-        let out_frames = resampler.output_frames_next();
-        match time_scaler.pop_frames(in_frames) {
-            Ok(block) => {
-                let out_block = resampler.process(&block, None).unwrap();
-                wav::write_frames(&mut writer, out_block, out_frames);
+                if exhausted {
+                    break;
+                }
             }
-            Err(InsufficientInputError) => break,
-        }
 
+            loop {
+                let in_frames = resampler.input_frames_next();
+                let out_frames = resampler.output_frames_next();
+                match time_scaler.pop_frames(in_frames) {
+                    Ok(block) => {
+                        let out_block = resampler.process(&block, None).unwrap();
+                        wav::write_frames(&mut writer, out_block, out_frames);
+                    }
+                    Err(InsufficientInputError) => break,
+                }
+
+            }
+
+            writer.finalize().unwrap();
+
+            println!("successfully transposed {input_file}, saving to {output_file}");
+        },
+        Command::Serve { bind } => {
+            println!("TODO: start server at {bind}");
+        },
     }
-
-    writer.finalize().unwrap();
-
-    println!("successfully transposed {file_path}, saving to {output_file}");
 }
 
